ffa3c3656b122d2f51d16769ae6b2c415bcea2d9
justalisteningman <stoikal@gmail.com>
2013-11-13 08:37:19 -0800

    changed how root loop collection is loaded

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
   def root
-    katie = User.includes(:loop_collections).find_by_username('milliemelinda')
+    
+    katie_usernames = ['barnaclered', 'milliemelinda']
+    
+    katie_usernames.each do |un|
+      katie ||= User.includes(:loop_collections).find_by_username('milliemelinda')
+    end
+    
     collection = (katie && katie.loop_collections.first) ? katie.loop_collections.first : LoopCollection.first
     
     @serialized_loop_collection = LoopCollection.active_model_serializer.new(
