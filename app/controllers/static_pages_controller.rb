ad18a9477f0a9a3fe21b6385ff7254204e51de8c
justalisteningman <stoikal@gmail.com>
2013-11-13 09:49:41 -0800

    fixed bug in pulling inital data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,12 @@ class StaticPagesController < ApplicationController
     katie_usernames = ['barnaclered', 'milliemelinda', 'melinda']
     katie = nil
     katie_usernames.each do |un|
-      katie ||= User.includes(:loop_collections).find_by_username('milliemelinda')
+      katie ||= User.includes(:loop_collections).find_by_username(un )
     end
     
     collection = (katie && katie.loop_collections.first) ? katie.loop_collections.first : LoopCollection.first
+    
+
     
     @serialized_loop_collection = LoopCollection.active_model_serializer.new(
                                   collection, scope: serialization_scope)
